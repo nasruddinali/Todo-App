5f44e60a50cbf0941106308de29d0c2cceddfaae
Nasruddin Ali <nasruddinali9246@gmail.com>
2019-07-16 12:47:08 +0530

    nodified

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { ListItem,List,  IconButton, Checkbox } from '@material-ui/core';
 
 export default props => (
-  <List className="tabtask" style={{ display: "flex", justifyContent: "center" }}>
-    <ListItem style={{textDecoration: props.todo.complete ? "line-through" : "" }} onClick={props.toggleComplete}>
+  <div className="tabtask" >
+    <list style={{textDecoration: props.todo.complete ? "line-through" : "" }} onClick={props.toggleComplete}>
       {props.todo.text}
-    </ListItem>
-    <Checkbox tabIndex={-1}  disable ripple onClick={props.toggleComplete} />
+    </list>
+    <input type="checkbox" className= "checkbox"  onClick={props.toggleComplete} />
     <button className="deletebutton" onClick={props.onDelete}>x</button>
-  </List>
+  </div>
 );
